60cd06a389d8b0be9b888004b43d09606fa73f3a
IPodstolnyy <ipodstolnyy@spb.beeline.ru>
2026-03-01 11:03:09 +0300

    ix admin pages load time.

--- blog/admin.py
+++ blog/admin.py
@@ -2,6 +2,19 @@ from django.contrib import admin
 from blog.models import Post, Tag, Comment
 
 
-admin.site.register(Post)
-admin.site.register(Tag)
-admin.site.register(Comment)
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    raw_id_fields = ('author', 'tags', 'likes')
+
+    list_display = ('title', 'author', 'published_at')
+
+
+@admin.register(Comment)
+class CommentsAdmin(admin.ModelAdmin):
+    raw_id_fields = ('post', 'author')
+    list_display = ('post', 'author', 'published_at')
+
+
+@admin.register(Tag)
+class TagAdmin(admin.ModelAdmin):
+    search_fields = ('title',)
